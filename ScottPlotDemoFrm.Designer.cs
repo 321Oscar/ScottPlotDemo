d54443fc3ee00d334793f933acc495ab1cdd04dc
卫东 徐 <975867938@qq.com>
2025-01-09 18:48:09 +0800

    添加项目文件。

---
+++ ScottPlotDemoFrm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ScottPlotDemo
+{
+    partial class ScottPlotDemoFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.rbDataLogger = new System.Windows.Forms.RadioButton();
+            this.rbDataStreamer = new System.Windows.Forms.RadioButton();
+            this.rbDataStreamerXY = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.DisplayScale = 0F;
+            this.formsPlot1.Location = new System.Drawing.Point(12, 55);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(619, 388);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(185, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // rbDataLogger
+            // 
+            this.rbDataLogger.AutoSize = true;
+            this.rbDataLogger.Location = new System.Drawing.Point(290, 24);
+            this.rbDataLogger.Name = "rbDataLogger";
+            this.rbDataLogger.Size = new System.Drawing.Size(95, 16);
+            this.rbDataLogger.TabIndex = 4;
+            this.rbDataLogger.TabStop = true;
+            this.rbDataLogger.Text = "radioButton1";
+            this.rbDataLogger.UseVisualStyleBackColor = true;
+            // 
+            // rbDataStreamer
+            // 
+            this.rbDataStreamer.AutoSize = true;
+            this.rbDataStreamer.Location = new System.Drawing.Point(391, 27);
+            this.rbDataStreamer.Name = "rbDataStreamer";
+            this.rbDataStreamer.Size = new System.Drawing.Size(95, 16);
+            this.rbDataStreamer.TabIndex = 5;
+            this.rbDataStreamer.TabStop = true;
+            this.rbDataStreamer.Text = "DataStreamer";
+            this.rbDataStreamer.UseVisualStyleBackColor = true;
+            // 
+            // rbDataStreamerXY
+            // 
+            this.rbDataStreamerXY.AutoSize = true;
+            this.rbDataStreamerXY.Location = new System.Drawing.Point(492, 24);
+            this.rbDataStreamerXY.Name = "rbDataStreamerXY";
+            this.rbDataStreamerXY.Size = new System.Drawing.Size(107, 16);
+            this.rbDataStreamerXY.TabIndex = 6;
+            this.rbDataStreamerXY.TabStop = true;
+            this.rbDataStreamerXY.Text = "DataStreamerXY";
+            this.rbDataStreamerXY.UseVisualStyleBackColor = true;
+            // 
+            // ScottPlotDemoFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 455);
+            this.Controls.Add(this.rbDataStreamerXY);
+            this.Controls.Add(this.rbDataStreamer);
+            this.Controls.Add(this.rbDataLogger);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.formsPlot1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "ScottPlotDemoFrm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton rbDataLogger;
+        private System.Windows.Forms.RadioButton rbDataStreamer;
+        private System.Windows.Forms.RadioButton rbDataStreamerXY;
+    }
+}
+
---
+++ ScottPlotDemoFrm.cs
@@ -0,0 +1,325 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlotDemo
+{
+    public partial class ScottPlotDemoFrm : Form
+    {
+        internal class TestModel : BindingBase
+        {
+            private double timeStamp;
+
+            public string Name { get; set; }
+            public string Value { get; set; }
+            /// <summary>
+            /// ms
+            /// </summary>
+            public double TimeStamp { get => timeStamp; set => SetProperty(ref timeStamp, value); }
+        }
+        private List<ScottPlot.Plottables.DataLogger> dataLoggers;
+        private List<ScottPlot.Plottables.DataStreamerXY> dataStreamerXYs;
+        private List<ScottPlot.Plottables.DataStreamer> dataStreamers;
+        List<ScottPlot.Plot> plots;
+        System.Windows.Forms.Timer UpdatePlotTimer = new System.Windows.Forms.Timer() { Interval = 50, Enabled = true };
+        IList<TestModel> models;
+        private int lineType = 2;
+
+        public ScottPlotDemoFrm()
+        {
+            InitializeComponent();
+            r = new Random();
+            dataLoggers = new List<ScottPlot.Plottables.DataLogger>();
+            dataStreamers = new List<ScottPlot.Plottables.DataStreamer>();
+            dataStreamerXYs = new List<ScottPlot.Plottables.DataStreamerXY>();
+            plots = new List<Plot>();
+
+            this.rbDataLogger.CheckedChanged += RbDataLogger_CheckedChanged;
+            this.rbDataStreamer.CheckedChanged += RbDataLogger_CheckedChanged;
+            this.rbDataStreamerXY.CheckedChanged += RbDataLogger_CheckedChanged;
+
+            models = GenerateModels(5);
+
+            this.Load += ScottPlotDemoFrm_Load;
+        }
+
+        private void RbDataLogger_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sender == rbDataLogger && rbDataLogger.Checked)
+            {
+                Stop();
+                lineType = 0;
+                GeneratePlots(models);
+            }
+            else if (sender == rbDataStreamer && rbDataStreamer.Checked)
+            {
+                Stop();
+                lineType = 1;
+                GeneratePlots(models);
+            }
+            else if (sender == rbDataStreamerXY && rbDataStreamerXY.Checked)
+            {
+                Stop();
+                lineType = 2;
+                GeneratePlots(models);
+            }
+        }
+
+        private void ScottPlotDemoFrm_Load(object sender, EventArgs e)
+        {
+            GeneratePlots(models);
+
+            UpdatePlotTimer.Tick += (s, ee) =>
+            {
+                if (dataLoggers.Any(x => x.HasNewData)) formsPlot1.Refresh();
+                if (dataStreamers.Any(x => x.HasNewData)) formsPlot1.Refresh();
+                if (dataStreamerXYs.Any()) formsPlot1.Refresh();
+            };
+        }
+        void GeneratePlots(IEnumerable<TestModel> models)
+        {
+            ClearDataLine();
+            Plot plot = formsPlot1.Plot;
+            formsPlot1.Multiplot.Reset(plot);
+            var firstModel = models.First();
+
+            CreateDataLine(plot, firstModel, lineType);
+            foreach (var item in models.Skip(1))
+            {
+                plot = new Plot();
+                //CreateDataLogger(plot, item);
+                CreateDataLine(plot, item, lineType);
+                formsPlot1.Multiplot.AddPlot(plot);
+                plots.Add(plot);
+            }
+            formsPlot1.Multiplot.ShareX(plots);
+        }
+
+        IList<TestModel> GenerateModels(int count)
+        {
+            List<TestModel> models = new List<TestModel>();
+
+            for (int i = 0; i < count; i++)
+            {
+                TestModel model = new TestModel()
+                {
+                    Name = "Model" + i,
+                };
+                model.PropertyChanged += Model_PropertyChanged;
+                models.Add(model);
+            }
+
+            return models;
+        }
+
+        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(TestModel.TimeStamp))
+            {
+
+                var signal = sender as TestModel;
+                AddPoint(signal, lineType);
+                //ScottPlot.Plottables.DataLogger datalogger = dataLoggers.FirstOrDefault(x => x.LegendText == signal.Name);
+                //datalogger.Add(double.Parse(signal.Value)); 
+                //ScottPlot.Plottables.DataStreamer dataStreamer = dataStreamers.FirstOrDefault(x => x.LegendText == signal.Name);
+                //dataStreamer.Add(double.Parse(signal.Value));
+            }
+        }
+
+        void CreateDataLine(Plot plot, TestModel model, int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    CreateDataLogger(plot, model);
+                    break;
+                case 1:
+                    CreateDataStreamer(plot, model);
+                    break;
+                case 2:
+                    CreateDataStreamerXY(plot, model);
+                    break;
+            }
+        }
+
+        void ClearDataLine()
+        {
+            formsPlot1.Plot.Clear();
+            //dataLoggers.FirstOrDefault().d
+            dataLoggers.Clear();
+            dataStreamers.Clear();
+            dataStreamerXYs.Clear();
+            plots.Clear();
+        }
+
+        void AddPoint(TestModel model, int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    //CreateDataLogger(plot, model);
+                    ScottPlot.Plottables.DataLogger datalogger = dataLoggers.FirstOrDefault(x => x.LegendText == model.Name);
+                    datalogger.Add(double.Parse(model.Value));
+                    break;
+                case 1:
+                    //CreateDataStreamer(plot, model);
+                    ScottPlot.Plottables.DataStreamer dataStreamer = dataStreamers.FirstOrDefault(x => x.LegendText == model.Name);
+                    dataStreamer.Add(double.Parse(model.Value));
+                    break;
+                case 2:
+                    //CreateDataStreamerXY(plot, model);
+                    ScottPlot.Plottables.DataStreamerXY dataStreamerxy = dataStreamerXYs.FirstOrDefault(x => x.LegendText == model.Name);
+                    dataStreamerxy.Add(model.TimeStamp, double.Parse(model.Value));
+                    break;
+            }
+        }
+
+        void CreateDataLogger(Plot plot, TestModel model)
+        {
+            ScottPlot.Plottables.DataLogger dataLogger = plot.Add.DataLogger();
+            plot.YLabel(model.Name);
+            //plot1.Axes.Left
+            dataLogger.ViewSlide();
+            dataLogger.LegendText = model.Name;
+            dataLoggers.Add(dataLogger);
+        }
+        //private List<ScottPlot.Plottables.DataStreamer> dataStreamers;
+        void CreateDataStreamer(Plot plot, TestModel model)
+        {
+            ScottPlot.Plottables.DataStreamer dataStreamer = plot.Add.DataStreamer(1000);
+            plot.YLabel(model.Name);
+            dataStreamer.ViewScrollLeft();
+            dataStreamer.LegendText = model.Name;
+            dataStreamers.Add(dataStreamer);
+        }
+
+        void CreateDataStreamerXY(Plot plot, TestModel model)
+        {
+            ScottPlot.Plottables.DataStreamerXY dataStreamer = plot.Add.DataStreamerXY(1000);
+            plot.YLabel(model.Name);
+            //dataStreamer.ViewScrollLeft();
+            dataStreamer.LegendText = model.Name;
+            dataStreamerXYs.Add(dataStreamer);
+        }
+
+        Thread thread;
+        private bool dataChanging;
+        private Random r;
+        private DateTime startTime;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //start change value
+            foreach (var item in dataLoggers)
+            {
+                item.Clear();
+            }
+            foreach (var item in dataStreamers)
+            {
+                item.Clear(double.NaN);
+            }
+            dataChanging = true;
+            startTime = DateTime.Now;
+            thread = new Thread(DataChangeLoop);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void DataChangeLoop()
+        {
+            do
+            {
+                foreach (var model in models)
+                {
+                    model.Value = (r.NextDouble() * 100).ToString("f2");
+                    model.TimeStamp = (DateTime.Now - startTime).TotalMilliseconds;
+                }
+
+                Thread.Sleep(10);
+            } while (dataChanging);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        void Stop()
+        {
+            dataChanging = false;
+            if (thread != null)
+                thread.Join();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //add a new Model and plot
+            int count = models.Count;
+            var newModel = new TestModel()
+            {
+                Name = "Model" + count,
+            };
+            newModel.PropertyChanged += Model_PropertyChanged;
+            models.Add(newModel);
+            //add plot
+            Plot p = new Plot();
+            //CreateDataLogger(p, newModel);
+            CreateDataLine(p, newModel, lineType);
+            formsPlot1.Multiplot.AddPlot(p);
+            plots.Add(p);
+        }
+    }
+
+    public abstract class BindingBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public SynchronizationContext UIContext;
+        protected virtual bool SetProperty<T>(ref T storage, T value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+        {
+            if (UIContext != null)
+            {
+                UIContext.Post(_ =>
+                {
+                    OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+                }, null);
+
+            }
+            else
+            {
+                OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            try
+            {
+                //PropertyChanged?.BeginInvoke(this,args,null,null);
+                this.PropertyChanged?.Invoke(this, args);
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+
+    }
+}
